cd32c70d275a67202962627791ed0a715ad7efb5
vy3191 <venkyafhs@gmail.com>
2020-05-05 19:25:54 -0400

    Added more files structures

---
+++ src/components/landingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+
+function LandingPage(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/components/registration/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+function Registration(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Registration;
