2e34b20d6a7da6ca72f90a554277e83e3a6c9f89
farjadfazli <farjadfazli@gmail.com>
2020-05-27 22:06:26 -0400

    troubleshooting handleSubmit

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -1,44 +1,60 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { axiosWithAuth } from "../apiStuff/axiosWithAuth";
+import axios from 'axios';
 import { UserContext } from '../../UserContext'
 import "./styles.scss";
 import Header from "../header/Header";
 
 const Registration = (props) => {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => handleOnSubmit(data);
+  // const onSubmit = (data) => handleOnSubmit(data);
   const { user, setUser } = useContext(UserContext);
+  const [newUser, setNewUser] = useState({
+    email: "",
+    password: "",
+    user_type: ""
+  })
   //console.log(errors);
 
-  const handleOnSubmit = (props) => {
-    let userAuth = {
-      email: props.email,
-      user_type: props.user_type,
-      password: props.password,
-    };
-    axiosWithAuth()
-    .post("/auth/register", userAuth)
-    .then((res) => {
-      console.log(res)
-      window.localStorage.setItem("token", res.data.token)
-      console.log(user)
-      window.location = "/profile"
+  const handleChange = event => {
+    setNewUser({
+      ...newUser,
+      [event.target.name]: event.target.value
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log(newUser)
+  }
+
+  const handleSubmit2 = event => {
+    event.preventDefault()
+    axios.post('https://niyon-app.herokuapp.com/auth/register', newUser)
+      .then(res => {
+        console.log(res.data.user.id)
+        console.log(user)
+        setTimeout(
+          setUser({
+            id: res.data.user.id
+          }), 1000
+        )
+        window.localStorage.setItem("token", res.data.token)
+        setTimeout(
+          console.log(user), 2000
+        )
+      })
+      .catch(err => console.log(err))
   }
 
   return (
     <div className="formWrap">
       <Header />
-      <form className="formRegister" onSubmit={handleSubmit(onSubmit)}>
+      <form className="formRegister" onSubmit={handleSubmit2}>
         <input
           type="email"
           placeholder="Email"
           name="email"
           ref={register({ required: true })}
+          onChange={handleChange}
+          value={newUser.email}
         />
         {errors.email && (
           <p style={{ color: "orange", marginTop: 10 }}>"Email is required"</p>
@@ -48,6 +64,8 @@ const Registration = (props) => {
           placeholder="Password"
           name="password"
           ref={register({ required: true })}
+          onChange={handleChange}
+          value={newUser.password}
         />
         {errors.password && (
           <p style={{ color: "orange", marginTop: 10 }}>
@@ -69,7 +87,12 @@ const Registration = (props) => {
             "Passwords do not match"
           </p>
         )}
-        <select name="user_type" ref={register({ required: true })}>
+        <select 
+          name="user_type" 
+          ref={register({ required: true })}
+          onChange={handleChange}
+          value={newUser.user_type}
+        >
           <option value="Mentor">Mentor</option>
           <option value=" Mentee">Mentee</option>
         </select>
