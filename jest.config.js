8b23e2c9323256ebacc8315feb3fbfcac0639eeb
farjadfazli <farjadfazli@gmail.com>
2020-06-04 19:24:22 -0400

    added jest.config.js and updated package.json as per Don

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+{
+    "setupFiles": ["<rootDir>/src/setupTests.js"],
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.js?$",
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+      "**/*.{js,jsx}",
+      "!**/node_modules/**",
+      "!**/vendor/**",
+      "!**/_tests__/**",
+      "!**/*.{spec,test}.{js,jsx}",
+      "!**/coverage/**"
+    ],
+    "coverageReporters": ["lcov", "json-summary", "text"],
+    "moduleDirectories": ["node_modules", "src"]
+  }
